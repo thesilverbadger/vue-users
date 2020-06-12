432fa6e4ad7c6b8b40e614242a2fc624632959a6
Tom Wire <tom.w.wire@gmail.com>
2020-06-12 22:40:02 +0100

    Get some users!

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
     <h2>Users</h2>
+    <div v-bind:key="user.id" v-for="user in users">
+      <h4>{{ user.givenName }}</h4>
+    </div>
   </div>
 </template>
 
 <script>
+import Axios from "axios";
+
 export default {
-  name: "Users"
+  name: "Users",
+  data() {
+    return {
+      users: []
+    };
+  },
+  methods: {},
+  created() {
+    const token = localStorage.getItem("jwt");
+    console.log(token);
+    Axios.get("https://localhost:5001/api/users", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then(response => {
+        this.users = response.data;
+      })
+      .catch(function(error) {
+        console.error(error.response);
+      });
+  }
 };
 </script>
