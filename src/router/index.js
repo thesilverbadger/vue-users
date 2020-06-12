0cd7ba0534f2a11b23652de836eaa3a7ace0b3e9
Tom Wire <tom.w.wire@gmail.com>
2020-06-12 20:45:20 +0100

    Add some roots

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Index from "../views/Index.vue";
+import Login from "../views/Login.vue";
+import Users from "../views/Users.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +20,19 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/users",
+    name: "Users",
+    component: Users,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
