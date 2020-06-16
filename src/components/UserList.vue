789301bfaef57045552903d6c458b7d18650bbf1
Tom Wire <tom.w.wire@gmail.com>
2020-06-16 14:23:16 +0100

    Create a UserList component for managing a list of users

---
+++ src/components/UserList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <table class="table table-bordered table-striped">
+      <thead>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Email</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-bind:key="user.id" v-bind:item="user" v-for="user in users">
+          <td>{{ user.givenName }}</td>
+          <td>{{ user.familyName }}</td>
+          <td>{{ user.email }}</td>
+          <td>
+            <router-link :to="{ name: 'UsersEdit', params: { id: user.id } }">Edit</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import dataservice from "../services/data.service";
+export default {
+  name: "UserList",
+  data() {
+    return {
+      users: []
+    };
+  },
+  async created() {
+    this.users = await dataservice.get("api/users");
+  }
+};
+</script>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,42 +1,17 @@
 <template>
   <div>
     <h2>Users</h2>
-    <table class="table table-bordered table-striped">
-      <thead>
-        <tr>
-          <th>Firstname</th>
-          <th>Lastname</th>
-          <th>Email</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-bind:key="user.id" v-bind:item="user" v-for="user in users">
-          <td>{{ user.givenName }}</td>
-          <td>{{ user.familyName }}</td>
-          <td>{{ user.email }}</td>
-          <td>
-            <router-link :to="{ name: 'UsersEdit', params: { id: user.id } }">Edit</router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <UserList />
   </div>
 </template>
 
 <script>
-import dataservice from "../services/data.service";
+import UserList from "../components/UserList";
 
 export default {
   name: "Users",
-  data() {
-    return {
-      users: []
-    };
-  },
-  methods: {},
-  async created() {
-    this.users = await dataservice.get("api/users");
+  components: {
+    UserList
   }
 };
 </script>
