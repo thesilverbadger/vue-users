39e8358090e526ca5be955102f252601ccc36496
Tom Wire <tom.w.wire@gmail.com>
2020-06-17 10:16:16 +0100

    Pop some text on the home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>Home</h1>
+    <p>
+      Simple frontend over an equally simple User Api. API project can be found
+      <a
+        href="https://github.com/thesilverbadger/UserApi"
+      >here</a>.
+    </p>
   </div>
 </template>
 
